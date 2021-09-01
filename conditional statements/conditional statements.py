8d9f21c96964fed434c81d58df8a7026e94c17d9
Manisha <manishayerandekar727@gmail.com>
2021-09-01 17:44:19 +0530

    changes in logic

--- conditional statements/conditional statements.py
+++ conditional statements/conditional statements.py
@@ -1,11 +1,21 @@
 print("program to convert string in uppercase or lowercase")
 string_required=input("please enter your string")
-case_required=input("please enter whether you want upper case or lower case")
-check_case=string_required.islower()
-if [check_case is True  
-    print("the string is already lower case.\nThank you")
+case_required=input("please enter whether you want UPPER CASE or lower case say UPPER CASE or say lower case")
+if string_required.islower():
+    if case_required == "lower case":
+        print("the string you entered is already lowercase")
+    else:
+        if case_required=="UPPER CASE":
+            print(f"the upper case of your string is {string_required.upper()}")
 else:
-    print(f"the upper case of yur string is {string_required.upper()}")
+    if string_required.isupper():
+        if case_required == "UPPER CASE":
+            print("the string you entered is already uppercase")
+        else:
+            if case_required == "lower case":
+                print(f"the lowercase of your string is {string_required.lower()} ")
+
+
 
 
 
